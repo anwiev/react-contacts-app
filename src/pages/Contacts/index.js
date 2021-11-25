f73cd26602c77302e131e49ddbe673f9c9b9a126
anwiev <anwiev@gmail.com>
2021-11-26 01:30:43 +0200

    added statuses, array with data, displayed first arr. element

--- src/pages/Contacts/index.js
+++ src/pages/Contacts/index.js
@@ -1,3 +1,37 @@
+import { useState, useEffect } from "react";
+
 export const Contacts = () => {
-  return <div>Contacts</div>;
+  // array where we get contacts
+  const [contacts, setContacts] = useState([]);
+  // loading status
+  const [isLoading, setIsLoading] = useState(true);
+  // error status
+  const [isError, setIsError] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://randomuser.me/api/?results=200")
+      .then((response) => response.json())
+      .then(({ results }) => {
+        setContacts(results);
+        setIsLoading(false);
+        setIsError(false);
+        console.log(results);
+      })
+      .catch(() => {
+        setIsLoading(false);
+        setIsError(true);
+      });
+  }, []);
+
+  // build interface
+  if (isLoading) {
+    return <div>...loading</div>;
+  }
+
+  if (isError) {
+    return <div>...error</div>;
+  }
+
+  return <div>Contacts {contacts[0].name.first}</div>;
 };
